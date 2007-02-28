ef56a175871a30281362a4b7f7f9e619e24b6ff8
Marc Maurer <uwog@uwog.net>
2007-02-28 22:22:20 +0100

    Update the (c) info

--- toolbar.py
+++ toolbar.py
@@ -1,4 +1,5 @@
-# Copyright (C) 2006, Red Hat, Inc.
+# Copyright (C) 2006 by Martin Sevior
+# Copyright (C) 2006-2007 Marc Maurer <uwog@uwog.net>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -13,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-#
+
 import logging
 import gtk
 import pango
