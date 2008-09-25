a35f9580688a3c0def7677dd9080f0c2f5d71eea
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2008-09-25 12:35:06 +0100

    _on_members_changed: ignore bus_name if it's None

--- AbiWordActivity.py
+++ AbiWordActivity.py
@@ -284,6 +284,9 @@ class AbiWordActivity (Activity):
         logger.debug("_on_members_changed")
         for handle in removed:
             bus_name = self.participants.pop(handle, None)
+            if bus_name is None:
+                continue
+
             logger.debug('removed handle: %d, with dbus name: %s', handle,
                          bus_name)
             self.abiword_canvas.invoke_cmd('com.abisource.abiword.abicollab.olpc.buddyLeft', bus_name, 0, 0)
