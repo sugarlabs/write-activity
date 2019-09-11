sugarlabs/write-activity

commit d3bc9756f1fdc9484f75bb450c3c525625278d15

Port to Python 3
Tested-by: Rahul Bothra <f2016015@pilani.bits-pilani.ac.in>  # Ubuntu 16.04
Signed-off-by: James Cameron <quozl@laptop.org>  # Ubuntu 18.04

AbiWordActivity.py
@@ -262,7 +262,7 @@ class AbiWordActivity(activity.Activity):
             return True
 
         pixbuf.save_to_callbackv(save_func, preview_data, 'png', [], [])
-        preview_data = ''.join(preview_data)
+        preview_data = ''.join(map(str,preview_data)).encode()
 
         return preview_data
 

activity/activity.info
@@ -1,7 +1,7 @@
 [Activity]
 name = Write
 bundle_id = org.laptop.AbiWordActivity
-exec = sugar-activity AbiWordActivity.AbiWordActivity
+exec = sugar-activity3 AbiWordActivity.AbiWordActivity
 icon = activity-write
 activity_version = 100
 max_participants = 6

fontcombobox.py
@@ -75,7 +75,7 @@ class FontComboBox(Gtk.ToolItem):
         else:
             subcell_size = 11
         radius = 2 * subcell_size
-        theme = "GtkButton {border-radius: %dpx;}" % radius
+        theme = b"GtkButton {border-radius: %dpx;}" % radius
         css_provider = Gtk.CssProvider()
         css_provider.load_from_data(theme)
         style_context = bt.get_style_context()
@@ -248,7 +248,7 @@ class FontSize(Gtk.ToolItem):
         hbox.pack_start(self._size_up, False, False, 5)
 
         radius = 2 * subcell_size
-        theme_up = "GtkButton {border-radius:0px %dpx %dpx 0px;}" % (radius,
+        theme_up = b"GtkButton {border-radius:0px %dpx %dpx 0px;}" % (radius,
                                                                      radius)
         css_provider_up = Gtk.CssProvider()
         css_provider_up.load_from_data(theme_up)
@@ -257,7 +257,7 @@ class FontSize(Gtk.ToolItem):
         style_context.add_provider(css_provider_up,
                                    Gtk.STYLE_PROVIDER_PRIORITY_USER)
 
-        theme_down = "GtkButton {border-radius: %dpx 0px 0px %dpx;}" % (radius,
+        theme_down = b"GtkButton {border-radius: %dpx 0px 0px %dpx;}" % (radius,
                                                                         radius)
         css_provider_down = Gtk.CssProvider()
         css_provider_down.load_from_data(theme_down)

gridcreate.py
@@ -132,7 +132,7 @@ class GridCreateTest(Gtk.Window):
         self.show_all()
 
     def __create_table(self, grid_creator, rows, columns):
-        print 'rows %d columns %d' % (rows, columns)
+        print('rows %d columns %d' % (rows, columns))
 
 if __name__ == '__main__':
     GridCreateTest()

speech.py
@@ -27,12 +27,12 @@ try:
     Gst.ElementFactory.make('espeak', None)
     from speech_gst import *
     _logger.error('use gst-plugins-espeak')
-except Exception, e:
+except Exception as e:
     _logger.error('disable gst-plugins-espeak: %s' % e)
     try:
         from speech_dispatcher import *
         _logger.error('use speech-dispatcher')
-    except Exception, e:
+    except Exception as e:
         supported = False
         _logger.error('disable speech: %s' % e)
 

speech_dispatcher.py
@@ -33,7 +33,7 @@ def voices():
         voices = client.list_synthesis_voices()
         client.close()
         return voices
-    except Exception, e:
+    except Exception as e:
         _logger.warning('speech dispatcher not started: %s' % e)
         return []
 
@@ -46,7 +46,7 @@ def say(words):
         client.set_language(speech.voice[1])
         client.speak(words)
         client.close()
-    except Exception, e:
+    except Exception as e:
         _logger.warning('speech dispatcher not running: %s' % e)
 
 
@@ -91,14 +91,14 @@ class EspeakThread(threading.Thread):
                 time.sleep(0.1)
             self.cancel()
             self.client.close()
-        except Exception, e:
+        except Exception as e:
             _logger.warning('speech-dispatcher client not created: %s' % e)
 
     def cancel(self):
         if self.client:
             try:
                 self.client.cancel()
-            except Exception, e:
+            except Exception as e:
                 _logger.warning('speech dispatcher cancel failed: %s' % e)
 
     def next_word_cb(self, type, **kargs):

toolbar.py
@@ -25,7 +25,7 @@ from gi.repository import GObject
 
 import os
 import tempfile
-from urlparse import urlparse
+from urllib.parse import urlparse
 
 from sugar3.graphics.toolbutton import ToolButton
 from sugar3.graphics.toolcombobox import ToolComboBox

commit 52e15c4c365b90d4a8894207215b4323cc20348c

Fix broken previews
Previews did contain Python quoted strings instead of PNG.

Regression introduced by d3bc975 ("Port to Python 3").

AbiWordActivity.py
@@ -257,12 +257,12 @@ class AbiWordActivity(activity.Activity):
 
         preview_data = []
 
-        def save_func(buf, lenght, data):
+        def save_func(buf, length, data):
             data.append(buf)
             return True
 
         pixbuf.save_to_callbackv(save_func, preview_data, 'png', [], [])
-        preview_data = ''.join(map(str,preview_data)).encode()
+        preview_data = b''.join(preview_data)
 
         return preview_data
 