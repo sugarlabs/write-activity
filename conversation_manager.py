2be735217c72b8f3f31141ca393a70a9f2927bf9
Aman Naik <amancodes247@gmail.com>
2026-01-31 23:26:17 +0530

    Fix: Added new line at EOF, license excerpt and copyright

--- conversation_manager.py
+++ conversation_manager.py
@@ -1,3 +1,21 @@
+# Copyright (C) 2006 by Martin Sevior
+# Copyright (C) 2006-2007 Marc Maurer <uwog@uwog.net>
+# Copyright (C) 2007, One Laptop Per Child
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
 import json
 import os
 from sugarai_api import get_llm_response, get_llm_response_framework
@@ -81,3 +99,4 @@ class ConversationContext:
 
     def update_story_info(self):
         self.story_info = extract_story_info(self.messages)
+  
